6cb49d858b98a4a051b011f8fd7c4a561115641b
ctdupuis <ctdupuis7@gmail.com>
2020-04-07 21:22:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+const array = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+const string = 'Bobby';
+
+function findMatching(array, driver) {
+    return array.filter( d => {
+        return d.toLowerCase() == driver.toLowerCase()
+    }) 
+}
+
+function fuzzyMatch(array, driver) {
+    return array.filter( d => {
+        return d[0].toLowerCase() == driver[0].toLowerCase()
+    })
+}
+
+function matchName (array, driver) {
+    return array.filter(d => {
+       return d.name.toLowerCase() == driver.toLowerCase()
+    })
+}
