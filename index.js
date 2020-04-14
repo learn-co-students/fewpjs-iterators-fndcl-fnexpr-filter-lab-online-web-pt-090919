8473e166e6afd01d654fbe6ca9ca6c6770498f5d
kalmegati <kalmegati@gmail.com>
2020-04-13 23:30:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(list, target) {
+    return list.filter(n => {
+        return n.toLowerCase() === target.toLowerCase();
+    });
+};
+
+function fuzzyMatch(list, target) {
+    return list.filter(n => {
+        return n.substr(0, target.length) === target;
+    });
+};
+
+function matchName(list, target) {
+    return list.filter(n => {
+        return n.name === target;
+    });  
+};
